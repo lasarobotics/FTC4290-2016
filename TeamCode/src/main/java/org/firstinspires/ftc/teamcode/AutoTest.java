0b28bb20743fed6692fa16c0d340a7a80954275c
Ojas Ahuja <ojas@ahujas.org>
2016-11-15 22:36:22 -0600

    Autonomous and TeleOp updates from competition

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoTest.java
@@ -0,0 +1,54 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.Range;
+import com.qualcomm.robotcore.hardware.GyroSensor;
+import com.qualcomm.robotcore.hardware.Servo;
+import android.os.Handler;
+
+import static android.os.SystemClock.sleep;
+
+@Autonomous(name = "autonomous", group = "test")
+
+public class AutoTest extends OpMode {
+    DcMotor back_left, front_left, back_right, front_right;
+    
+    public void init() {
+        back_left = hardwareMap.dcMotor.get("back_left");
+        int back_left_position = back_left.getCurrentPosition();
+        telemetry.addData("Back Left Position", back_left_position);
+
+        front_left = hardwareMap.dcMotor.get("front_left");
+        int front_left_position = front_left.getCurrentPosition();
+        telemetry.addData("Front Left Position", front_left_position);
+
+        back_right = hardwareMap.dcMotor.get("back_right");
+        int back_right_position = back_right.getCurrentPosition();
+        telemetry.addData("Back Right Position", back_right_position);
+
+        front_right = hardwareMap.dcMotor.get("front_right");
+        int front_right_position = front_right.getCurrentPosition();
+        telemetry.addData("Front Right Position", front_right_position);
+    }
+
+    public void loop() {
+        back_left.setPower(-(double) 1);
+        front_left.setPower(-(double) 1);
+        back_right.setPower((double) 1);
+        front_right.setPower((double) 1);
+        if ( Math.abs(front_right.getCurrentPosition()) > 10 * 1000 )
+            sleep(30000);
+
+    }
+
+    public void stop() {
+        back_left.setPower(0);
+        front_left.setPower(0);
+        back_right.setPower(0);
+        front_right.setPower(0);
+    }
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Blank.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import static android.os.SystemClock.sleep;
+
+@Autonomous(name = "do nothing", group = "test")
+
+public class Blank extends OpMode {
+
+    
+    public void init() {
+
+    }
+
+    public void loop() {
+
+    }
+
+    public void stop() {
+
+    }
+
+}
