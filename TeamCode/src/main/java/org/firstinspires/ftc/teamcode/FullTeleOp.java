6da8879c4c0a380605bceaf79c2e2fa44a35c960
Ojas Ahuja <oja@users.noreply.github.com>
2017-01-22 18:54:07 -0600

    Update FullTeleOp

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FullTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FullTeleOp.java
@@ -7,9 +7,8 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 @TeleOp(name = "full teleop", group = "test")
 public class FullTeleOp extends OpMode {
     DcMotor back_left, front_left, back_right, front_right, shooter, intake;
-    private float deadzone = 0.05f;    
-    
-    public bool direction = True;//true-beacon pusher is forward, false-intake is forward
+    private float deadzone = 0.05f;
+    int multiplier = 1;
 
     public void init() {
         back_left = hardwareMap.dcMotor.get("back_left");
@@ -22,42 +21,22 @@ public class FullTeleOp extends OpMode {
     }
 
     public void loop() {
-            if(direction){
-                telemetry.addData("direction: forward")
-            } else {
-                telemetry.addData("direction: reverse")
-            }
-        if(!direction){
-            back_left.setDirection(DcMotor.Direction.FORWARD);
-            front_left.setDirection(DcMotor.Direction.FORWARD);
-            back_right.setDirection(DcMotor.Direction.REVERSE);
-            front_right.setDirection(DcMotor.Direction.REVERSE);
-        } else{
-            back_left.setDirection(DcMotor.Direction.REVERSE);
-            front_left.setDirection(DcMotor.Direction.REVERSE);
-            back_right.setDirection(DcMotor.Direction.FORWARD);
-            front_right.setDirection(DcMotor.Direction.FORWARD);
-        }
-            
+
         if ((Math.abs(gamepad1.left_stick_x) < 0.7) && (Math.abs(gamepad1.right_stick_x) < 0.7)) {
-            back_left.setPower((double) removeDeadzone(deadzone, gamepad1.left_stick_y));
-            front_left.setPower((double) removeDeadzone(deadzone, gamepad1.left_stick_y));
-            back_right.setPower((double) removeDeadzone(deadzone, gamepad1.right_stick_y));
-            front_right.setPower((double) removeDeadzone(deadzone, gamepad1.right_stick_y));
+            back_left.setPower(multiplier * -(double) removeDeadzone(deadzone, gamepad1.left_stick_y));
+            front_left.setPower(multiplier * -(double) removeDeadzone(deadzone, gamepad1.left_stick_y));
+            back_right.setPower(multiplier * (double) removeDeadzone(deadzone, gamepad1.right_stick_y));
+            front_right.setPower(multiplier * (double) removeDeadzone(deadzone, gamepad1.right_stick_y));
         } else if ((gamepad1.left_stick_x + gamepad1.right_stick_x) / 2 < 0) { // left mecanum
-            back_left.setPower(-0.7);
-            front_left.setPower(0.7);
-            back_right.setPower(0.7);
-            front_right.setPower(-0.7);
+            back_left.setPower(multiplier * -0.7);
+            front_left.setPower(multiplier * 0.7);
+            back_right.setPower(multiplier * 0.7);
+            front_right.setPower(multiplier * -0.7);
         } else if ((gamepad1.left_stick_x + gamepad1.right_stick_x) / 2 > 0) { // right mecanum
-            back_left.setPower(0.7);
-            front_left.setPower(-0.7);
-            back_right.setPower(-0.7);
-            front_right.setPower(0.7);
-        }
-        
-        if(gamepad1.left_bumper){
-            direction = !direction;//reverses robot 'changes orientation'
+            back_left.setPower(multiplier * 0.7);
+            front_left.setPower(multiplier * -0.7);
+            back_right.setPower(multiplier * -0.7);
+            front_right.setPower(multiplier * 0.7);
         }
 
         if (gamepad2.y) {
